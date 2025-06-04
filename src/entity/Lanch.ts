3e9cc2686614822afa3f0e7212b3597b5566b2b3
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-06-04 16:07:09 -0300

    feat: Create Launch entity with validations

---
+++ src/entity/Lanch.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { IsDateString, IsEnum, IsNotEmpty, IsPositive } from "class-validator";
+
+export enum LaunchType {
+    CREDIT = "credit",
+    DEBIT = "debit",
+}
+
+@Entity()
+export class Launch {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    @IsDateString()
+    @IsNotEmpty()
+    date: string;  // ISO string date
+
+    @Column()
+    @IsNotEmpty()
+    description: string;
+
+    @Column("decimal", { precision: 10, scale: 2 })
+    @IsPositive()
+    amount: number;
+
+    @Column({
+        type: "enum",
+        enum: LaunchType,
+    })
+    @IsEnum(LaunchType)
+    type: LaunchType;
+}
